086efd7effe54bc03bdd40c13a497fa58b5753ad
JoaoSilvaDevel <joaogabriel975@hotmail.com>
2024-05-18 23:26:26 -0300

    Criado os forms com as telas

---
+++ TELAS/Resumo/resumo neil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.TELAS.Resumo
+{
+    public partial class resumo_neil : Form
+    {
+        public resumo_neil()
+        {
+            InitializeComponent();
+        }
+    }
+}
